b75f458e14ce6a8510d7ecb13b4d2e150aeb947c
Michael Kubiak <michael.kubiak@bath.edu>
2019-04-29 12:38:02 +0100

    started on allowing specific choice of c_alpha-c distance to be output

---
+++ distance.py
@@ -0,0 +1,36 @@
+# A module containing the functions for calculating the c-c_alpha distance
+#---------------------------------------------------------------------------------------------------------------------
+import re
+from math import sqrt
+
+def calculate_distance(cnum,content):
+    print("\n ")
+    print ("C alpha coordinates:")
+    ca = get_coords(re.finditer("(\d|-).{0,3}\.", content[cnum-1]), content[cnum-1])
+    print("C coordinates:")
+    c = get_coords(re.finditer("(\d|-).{0,3}\.", content[cnum]), content[cnum])
+    return sqrt(pow(c["x"]-ca["x"],2) + pow(c["y"]-ca["y"],2) + pow(c["z"]-ca["z"],2))
+
+
+def get_coords(nums, line):
+    c = {}
+    c["x"] = float(re.split(r"\s",line[nums.__next__().start():])[0])
+    c["y"] = float(re.split(r"\s",line[nums.__next__().start():])[0])
+    c["z"] = float(re.split(r"\s",line[nums.__next__().start():])[0])
+    print (c)
+    return c
+    
+def find_positions(content):    
+    lastLine = ""
+    atoms = []
+    firstatom = 0
+    for line in content:
+        if re.search("^ATOM",line):
+            
+            if re.search("^C\s",line[13:15]) and re.search("^CA\s",lastLine[13:16]):
+                m = re.search("\d", line)
+                atoms.append(re.split(r"\s",line[m.start():])[0])
+        elif (atoms == []):
+            firstatom += 1
+        lastLine = line
+    return [firstatom, atoms]
--- mini_project_solution.py
+++ mini_project_solution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #---------------------------------------------------------------------------------------------------------------------
 # A script to read the contents of a pdb file, and perform 3 operations:
-#   -   find the c-c_alpha distance for each residue in the structure
+#   -   find the c_alpha-c distance for each residue in the structure
 #   -   print the sequence provided in the pdb file with residues that are not included in the structure as lowercase
 #   -   ...TODO:
 #---------------------------------------------------------------------------------------------------------------------
@@ -21,19 +21,27 @@ def checkInput(input_file):
         return checkInput(input("The specified input file cannot be found, please provide another path:\n"))
 
 #---------------------------------------------------------------------------------------------------------------------
-# Parse command line arguments
+# Create Action class for the action that needs to be performed by the distance and all arguments (see parsing section)
 import argparse
+
+class distanceAction(argparse.Action):
+    def __init__(self,  option_strings, dest, nargs=2*'*',  **kwargs):
+        super(distanceAction,  self).__init__(option_strings,  dest,  **kwargs)
+    def __call__(self, parser, namespace, values, option_string = "A 1".split()):
+        print (option_string)
+#---------------------------------------------------------------------------------------------------------------------
+# Parse command line arguments
 # add an ArgumentParser object
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, 
 description ='''A script to read the contents of a pdb file, and perform 3 operations:
-\n\t-\tfind the c-c_alpha distance for each residue in the structure
+\n\t-\tfind the c_alpha-c distance for each residue in the structure
 \n\t-\tprint the sequence provided in the pdb file with residues that are not included in the structure as lowercase
 \n\t-\t...''')#TODO:
 # add each argument to the parser, along with the help line, for if the script is run with the -h argument, and the type of the argument
 # the input argument must be a string
 parser.add_argument("-i","--input",  help = "Path to input pdb file", type = str,  required = True)
 # -d, -s, and - TODO: append the correct integer to the perform list, which tell the program which of its functions to perform  
-parser.add_argument("-d", "--distance",  dest = "perform",  action="append_const", const = 1,  help="Perform the c-c_alpha distance calculation")
+parser.add_argument("-d", "--distance",  dest = "perform",  action="append_const", const = 1,  help="Perform the c_alpha-c distance calculation")
 parser.add_argument("-s", "--sequence",  dest = "perform", action = "append_const",  const = 2,  help="Print the sequence with residues not included in the structure shown as lowercase")
 #TODO: 3rd function
 # -a makes the perform list contain all 3 integers so that it will perform all three tasks
@@ -44,8 +52,15 @@ args = parser.parse_args()
 input_file = checkInput(args.input)
 
 #---------------------------------------------------------------------------------------------------------------------
+from distance import * 
 if 1 in args.perform:
-    ...#TODO: c-calpha
+    with open(input_file) as pdb:
+        content = pdb.readlines()
+        pos = find_positions(content)
+        firstatom = pos[0]
+        carbons = pos[1]
+        for c in carbons:
+            print(calculate_distance(int(c) + firstatom, content))
 if 2 in args.perform:
     ...#TODO: sequence
 if 3 in args.perform:
