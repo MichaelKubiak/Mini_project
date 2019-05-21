dbf860bcd3c46fa72f37c547a1d8b62e05b2f8f0
Michael Kubiak <michael.kubiak@bath.edu>
2019-05-21 14:01:05 +0100

    Start of new phase of work, after exams

--- distance.py
+++ distance.py
@@ -3,9 +3,10 @@
 import re
 from math import sqrt
 
+
 def calculate_distance(cnum,content):
     print("\n ")
-    print ("C alpha coordinates:")
+    print("C alpha coordinates:")
     ca = get_coords(re.finditer("(\d|-).{0,3}\.", content[cnum-1]), content[cnum-1])
     print("C coordinates:")
     c = get_coords(re.finditer("(\d|-).{0,3}\.", content[cnum]), content[cnum])
@@ -17,31 +18,35 @@ def get_coords(nums, line):
     c["x"] = float(re.split(r"\s",line[nums.__next__().start():])[0])
     c["y"] = float(re.split(r"\s",line[nums.__next__().start():])[0])
     c["z"] = float(re.split(r"\s",line[nums.__next__().start():])[0])
-    print (c)
+    print(c)
     return c
-    
-def find_positions(content):    
+
+
+def find_positions(content):
     lastLine = ""
     atoms = []
     firstatom = []
+    chains = []
     firstatom.append(0)
     newChain = False
-    i=0
+    i = 0
     for line in content:
-        if re.search("^ATOM",line):
-            if newChain == True:
+        if re.search("^ATOM", line):
+            if not line[21] in chains:
+                chains.append(line[21])
+            if newChain:
                 newChain = False
-                firstatom.append(firstatom[i-1] + int(re.search('\s\d+\s',line).group()))
-                print("First: ",firstatom[0], "\nThis: ", firstatom[1])
-            if re.search("^C\s",line[13:15]) and re.search("^CA\s",lastLine[13:16]):
+                firstatom.append(firstatom[i-1] + int(re.search('\s\d+\s', line).group()))
+            
+            if re.search("^C\s", line[13:15]) and re.search("^CA\s", lastLine[13:16]):
                 m = re.search("\d", line)
-                atoms.append(re.split(r"\s",line[m.start():])[0])
-        elif (atoms == []):
+                atoms.append(re.split(r"\s.", line[m.start():])[0])
+        elif atoms == []:
             firstatom[0] += 1
-        elif newChain == True:
-            break;
-        elif (re.search("^TER",line)):
+        elif newChain:
+            break
+        elif re.search("^TER", line):
             newChain = True
-            i+=1
+            i += 1
         lastLine = line
-    return [firstatom, atoms]
+    return [firstatom, chains, atoms]
